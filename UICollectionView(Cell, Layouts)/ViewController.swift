180fd12d9f46d5fff65490cfbfa668a564518ef1
sbaranchuk <sbaranchuk@protonmail.com>
2023-01-10 15:15:34 +0300

    Initial Commit

---
+++ UICollectionView(Cell, Layouts)/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UICollectionView(Cell, Layouts)
+//
+//  Created by Admin on 10/01/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
