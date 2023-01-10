59ba0782917cc83b691b5dea2824e5f12b453983
sbaranchuk <sbaranchuk@protonmail.com>
2023-01-10 16:53:08 +0300

    Folder, -Storyboard, Add window, clean code, SnapKit

---
+++ UICollectionView(Cell, Layouts)/Sources/App/AppDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.swift
+//  UICollectionView(Cell, Layouts)
+//
+//  Created by Admin on 10/01/2023.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
---
+++ UICollectionView(Cell, Layouts)/Sources/App/SceneDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  SceneDelegate.swift
+//  UICollectionView(Cell, Layouts)
+//
+//  Created by Admin on 10/01/2023.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        let viewController = FlowLayoutExampleViewController()
+        let navigationController = UINavigationController(rootViewController: viewController)
+        window = UIWindow(windowScene: windowScene)
+        window?.rootViewController = navigationController
+        window?.makeKeyAndVisible()
+    }
+}
+
