f81c38038cd8a09b1bd070094dfb31541411bc8a
sbaranchuk <sbaranchuk@protonmail.com>
2023-01-12 20:33:51 +0300

    Add three different types layouts.

--- UICollectionView(Cell, Layouts)/Sources/App/SceneDelegate.swift
+++ UICollectionView(Cell, Layouts)/Sources/App/SceneDelegate.swift
@@ -13,7 +13,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
-        let viewController = FlowLayoutExampleViewController()
+        let viewController = CompositionalView()
         let navigationController = UINavigationController(rootViewController: viewController)
         window = UIWindow(windowScene: windowScene)
         window?.rootViewController = navigationController
